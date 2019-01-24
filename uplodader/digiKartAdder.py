f9578d3d65298519aad68b04f8c7700c344eb538
Sethan <snooglecod@gmail.com>
2019-01-24 13:44:35 +0100

    Liten update la til areal

--- uplodader/digiKartAdder.py
+++ uplodader/digiKartAdder.py
@@ -2,18 +2,25 @@ import mysql.connector
 import re
 import csv
 
-with open("kommune.csv") as f:
+a=0
+with open("kommuneAreal.csv", encoding="ISO-8859-1") as f:
+    reader=csv.reader(f, delimiter=";", quotechar='"')
+    for a in reader:
+        print(a[2])
+
+
+with open("kommune.csv", encoding="ISO-8859-1") as f:
     reader=csv.reader(f, delimiter=";", quotechar='"')
     counter=0
     for row in reader:
-        
-        if counter<2:
-            print(row)
-        elif counter==50:
+        if counter==5:
             break
         else:
             print(re.findall(r'\D+',row[0]))
             print(re.findall(r'\d+',row[0]))
+
+            for x in range(2, len(row)-1):
+                print(row[x])
         counter+=1
 
 #mydb = mysql.connector.connect(
@@ -27,4 +34,4 @@ with open("kommune.csv") as f:
 #mycursor.execute("SHOW TABLES")
 #for x in mycursor:
  #   print(x)
-    
+
