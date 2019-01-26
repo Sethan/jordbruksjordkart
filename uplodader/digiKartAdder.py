3581a3c85eef90f76bd682e127a81d8e42a66b50
Safari Man <snooglecod@gmail.com>
2019-01-26 02:19:23 +0100

    fikset litt på db la til skript uploading scriptet kan fylle ut kommune og kommuneareal

--- uplodader/digiKartAdder.py
+++ uplodader/digiKartAdder.py
@@ -2,36 +2,42 @@ import mysql.connector
 import re
 import csv
 
-a=0
-with open("kommuneAreal.csv", encoding="ISO-8859-1") as f:
-    reader=csv.reader(f, delimiter=";", quotechar='"')
-    for a in reader:
-        print(a[2])
-
-
-with open("kommune.csv", encoding="ISO-8859-1") as f:
-    reader=csv.reader(f, delimiter=";", quotechar='"')
+with open("kommune.csv") as f:
+    reader=csv.reader(f, delimiter=";")
     counter=0
-    for row in reader:
-        if counter==5:
-            break
-        else:
-            print(re.findall(r'\D+',row[0]))
-            print(re.findall(r'\d+',row[0]))
+    mydb = mysql.connector.connect(
+            host="localhost",
+            user="digiKartAdmin",
+            passwd="CSOI-83ee-676e-991b-3f20-6a41",
+            database="mydb"
+    )
+    mycursor=mydb.cursor()
+    mycursor.execute("DELETE FROM kommune")
+    mycursor.execute("DELETE FROM kommunelandbruksareal")
+    mydb.commit()
+    years=[]
+    for row in reader:  
+        with open("kommuneAreal.csv", encoding="UTF-8") as u:
+            reader2=csv.reader(u, delimiter=";")
+            if counter==0:
+                for y in range(2,len(row)):
+                    a=re.findall(r'\d+',row[y])
+                    years.append(a[0])
+            elif counter==-3:
+                break
+            else:
+                for x in reader2:
+                       if x[0]==row[0]:
+                           sql = "INSERT INTO kommune (id,Navn,Areal) VALUES (%s,%s,%s)"
+                           values =(x[0],x[1],x[2])
+                           mycursor.execute(sql,values)
+                           mydb.commit()
+                           for n in range(3,len(row)):
+                               sql2 = "INSERT INTO kommunelandbruksareal (Landbruksareal,Aar, Kommune_id) VALUES (%s, %s, %s)"
+                               values2 = (row[n],years[n-3],x[0])
+                               mycursor.execute(sql2,values2)
+                               mydb.commit()
+                           break
+            counter+=1
 
-            for x in range(2, len(row)-1):
-                print(row[x])
-        counter+=1
-
-#mydb = mysql.connector.connect(
- # host="localhost",
-  #user="digiKartAdmin",
-  #passwd="CSOI-83ee-676e-991b-3f20-6a41",
-  #database="mydb"
-#)
-#mycursor = mydb.cursor()
-
-#mycursor.execute("SHOW TABLES")
-#for x in mycursor:
- #   print(x)
 
