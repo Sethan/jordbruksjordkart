116ebe4f51b700c4ed25c82e552182c3689e2c30
Sethan <snooglecod@gmail.com>
2019-02-11 14:56:33 +0100

    renders for 2017

--- html/js/app.js
+++ html/js/app.js
@@ -1,9 +1,23 @@
 
-var array = [];
+var ids=[];
+var percentages=[];
 
 function reqListener(data) {
-	array=this.response;
-	console.log(array);
+	var temp=this.response;
+	temp=temp.replace(/[^\d,.]/g, '');
+	var temparray=temp.split(",");
+	var n
+	for(n in temparray)
+	{
+		if(n%2==0)
+		{
+			ids.push(temparray[n]);
+		}
+		else {
+			percentages.push(temparray[n]);
+		}
+	}
+	load();
 }
 
 
@@ -14,13 +28,35 @@ oReq.send();
 
 
 
-var fylke;
-var kommune;
-var norge = document.getElementById('01').childNodes;
 
-for(fylke in norge){
-	if(norge[fylke].id>0)
+
+function load(){
+var i;
+for(i in ids){
+	var location;
+	var color=percentages[i]*1000+50;
+	if(ids[i]<1000)
+			{
+				location="0"+ids[i];
+			}
+		else
+		{
+				location=ids[i];
+		}
+
+	if(document.getElementById(location))
 	{
-			norge[fylke].style.fill="green";
+			document.getElementById(location).style.fill=rgb(0,color,0);
 	}
+
+}
+
+}
+
+
+function rgb(r, g, b){
+  r = Math.floor(r);
+  g = Math.floor(g);
+  b = Math.floor(b);
+  return ["rgb(",r,",",g,",",b,")"].join("");
 }
--- start.js
+++ start.js
@@ -3,7 +3,7 @@ var app = express();
 var db = require('./html/js/db_connect');
 
 var currentRes = [];
-db.query("SELECT * FROM kommune", function (err, result, fields) {
+db.query("SELECT id, (landbruksareal/(areal*1000.0)) as percent FROM kommune, kommunelandbruksareal where Aar=2017 and Kommune_id=id", function (err, result, fields) {
     if (err) throw err;
 	setValue(result);
   });
