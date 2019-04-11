bc39b9b1c9dd9864342de0960b81be31a35ad1fd
Sethan <snooglecod@gmail.com>
2019-04-11 11:03:22 +0200

    fixed some things needs more fixing 2

--- install.sh
+++ install.sh
@@ -9,14 +9,10 @@ apt-get install npm
 python3.7 -m pip install mysql-connector
 git init
 git clone https://github.com/Sethan/jordbruksjordkart.git
-
 mysql -e "source jordbruksjordkart/modeller/script.sql"
 mysql -e "GRANT ALL PRIVILEGES ON mydb.* TO 'digiKartAdmin'@'localhost' IDENTIFIED BY 'CSOI-83ee-676e-991b-3f20-6a41'"
-
-cd jordbruksjordkart/uplodader
-python3.7 digiKartAdder.py
-python3.7 digiKartLinker.py
-cd ..
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 npm install -g forever
 forever start jordbruksjordkart/start.js
+cd jordbruksjordkart/uplodader
+python3.7 digiKartAdder.py
